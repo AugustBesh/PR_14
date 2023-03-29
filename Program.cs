9848f67c693809ababdc444ef8b19edc1b55a456
AugustBesh <mr.besh.90@mail.ru>
2023-03-29 09:03:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
+int count=0;
+Console.WriteLine("Введите текст");
+string s= Console.ReadLine();
+string res = @"Input.txt";
+StreamReader sr = new StreamReader(res);
+string r = sr.ReadLine();
+bool result = Regex.IsMatch(s, r);
+Console.WriteLine($"Поиск слова {r}");
+if (result == true)
+{
+    count++;
+}
+if (count == 0)
+Console.WriteLine("Совпадений нет");
+else
+    Console.WriteLine($"Совпадений:{count}");
